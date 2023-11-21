75ce55b0c0540ebc14a96667a66fc019d10db4e0
JOSHUA BRONI <joshuabroni55@gmail.com>
2023-11-21 20:57:22 +0000

    updates

--- tests/testExample.spec.js
+++ tests/testExample.spec.js
@@ -4,7 +4,7 @@ import {test,expect} from "@playwright/test"
 
 test("checking for the existence of hubtel app on App store", async({page}) =>{  
 
-console.log( process.env.AUTH_TOKEN);
+// writing test to type slowly
 
 })
 
